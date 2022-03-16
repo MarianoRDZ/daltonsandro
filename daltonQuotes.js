4bd6cab04dbe0d818f30dd617787ae979cf7926a
Mariano Rodriguez <rdzc.mariano@gmail.com>
2022-03-16 14:38:47 -0300

    Removed modules

---
+++ daltonQuotes.js
@@ -0,0 +1,35 @@
+const daltonQuotes = [
+  "Cerrá el orto boludito",
+  "bot movements",
+  "choco la puta madre",
+  "Quién me presta cuenta?",
+  "nefasto...",
+  "pajarito",
+  "cerrá el orto",
+  "así no vas a mejorar",
+  "Rodro necesito que subas rápido de rango...",
+  "Qué te iba a decir...?",
+  "boludito",
+  "Soy daltónico, no pelotudo",
+  "Marian, sentate bien...",
+  "uuuuh, la boquearon estos novas...",
+  "Consigan gente!",
+  "Por eso no me gusta cuando no somos 5...",
+  "A pedazos",
+  "En breve Vicky sube a AK y desinstala",
+  "Rodro, haceme caso a las calls",
+  "Era eco Rodro...",
+];
+
+const daltonTips = [
+  "Rodro, holdeá ataúd",
+  "No te mostrés más!",
+  "Marian baiteá",
+  "RODRO APUNTA MAS ARRIBA",
+  "En este mapa es clave dominar banana",
+  "RODRO, COMPRÁ",
+  "Yo te miro palacio, vos mirame caverna",
+  "Subile el volúmen a tus auris",
+];
+
+module.exports = { daltonQuotes, daltonTips };
--- index.js
+++ index.js
@@ -1,94 +1,58 @@
-const Discord = require("discord.js");
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const { Client, MessageEmbed } = require("discord.js");
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const { daltonQuotes, daltonTips } = require("./daltonQuotes");
+require("dotenv").config();
 
-const daltonQuotes = [
-  "Cerrá el orto boludito",
-  "bot movements",
-  "choco la puta madre",
-  "Quién me presta cuenta?",
-  "nefasto...",
-  "pajarito",
-  "cerrá el orto",
-  "así no vas a mejorar",
-  "Rodro necesito que subas rápido de rango...",
-  "Qué te iba a decir...?",
-  "boludito",
-  "Soy daltónico, no pelotudo",
-  "Subile el volúmen a tus auris",
-  "Marian, sentate bien...",
-  "uuuuh, la boquearon estos novas...",
-  "Consigan gente!",
-  "Por eso no me gusta cuando no somos 5...",
-  "A pedazos",
-  "En breve Vicky sube a AK y desinstala",
-  "Rodro, haceme caso a las calls",
-];
-
-const daltonTips = [
-  "Rodro, holdeá ataúd",
-  "No te mostrés más!",
-  "Marian baiteá",
-  "RODRO APUNTA MAS ARRIBA",
-  "En este mapa es clave dominar banana",
-  "Era eco Rodro...",
-  "RODRO, COMPRÁ",
-  "Yo te miro palacio, vos mirame caverna",
-];
-
-const generateRandomQuote = () => {
-  return Math.floor(Math.random() * (daltonQuotes.length - 1) + 1);
-};
-
-const generateRandomTip = () => {
-  return Math.floor(Math.random() * (daltonTips.length - 1) + 1);
-};
-
-const generateBotMovement = (max, min) => {
-  return Math.floor(Math.random() * (max - min) + min);
+generateRandomNumber = (max, min) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 client.on("ready", () => {
-  console.log("Daltonto");
+  console.log("DaltonSandro is ALIVE!");
 });
 
 client.on("message", (msg) => {
   const botName = client.user.username;
-  if (msg.content === "!dalton") {
-    msg.reply(`El ${botName} te dice: ${daltonQuotes[generateRandomQuote()]}`);
-  }
-});
-
-client.on("message", (msg) => {
-  const botName = client.user.username;
-  if (msg.content === "!daltontips") {
-    msg.reply(`El ${botName} te dice: ${daltonTips[generateRandomTip()]}`);
-  }
-});
-
-client.on("message", (msg) => {
-  const botName = client.user.username;
-  const max = 10;
-  const min = 1;
-  const movement = generateBotMovement(max, min);
-
+  const movement = generateRandomNumber(10, 1);
   let frase;
 
-  if (movement < 3) {
-    frase = "Tremendo conito";
-  } else if (movement >= 3 && movement < 7) {
-    frase = "Bastante decente...";
-  } else if (movement >= 7 && movement < 10) {
-    frase = "Casi como yo papi eh, bien!";
-  } else {
-    frase = "GOD";
+  if (msg.content === "!dalton") {
+    msg.reply(
+      `El ${botName} te dice: ${
+        daltonQuotes[generateRandomNumber(daltonQuotes.length, 1)]
+      }`
+    );
+  }
+  if (msg.content === "!daltontips") {
+    const embed = new MessageEmbed()
+      .setColor("#ABCDAA")
+      .addField(
+        "Dalton TIP",
+        daltonTips[generateRandomNumber(daltonTips.length, 1)]
+      )
+      .setThumbnail(
+        "https://i.ibb.co/6sjmpMn/Whats-App-Image-2022-03-15-at-7-40-16-PM.jpg"
+      );
+
+    msg.channel.send({ embeds: [embed] });
   }
 
   if (msg.content === "!botrank") {
+    if (movement < 3) {
+      frase = "Tremendo conito";
+    } else if (movement >= 3 && movement < 7) {
+      frase = "Bastante decente...";
+    } else if (movement >= 7 && movement < 10) {
+      frase = "Casi como yo papi eh, bien!";
+    } else {
+      frase = "GOD";
+    }
+
     msg.reply(
-      `El ${botName} dice que tu nivel de bot es de '${movement}' sobre ${max}. ${frase}`
+      `El ${botName} dice que tu nivel de bot es de ${movement} sobre ${10}. ${frase}`
     );
   }
 });
 
 // Logineamos
-client.login("OTUzNDE0ODc2MTI5MjY3NzIy.YjEOzA.A7EkukK_NMmnMLuyCBCOI4g6wpE");
+client.login(process.env.DISCORD_TOKEN);
